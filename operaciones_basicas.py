27016cb171dfab606c149e492cc66a0450710e2d
juliangarcia3200 <juliangarcia3200@gmail.com>
2025-08-05 18:21:15 -0500

    Agrego multiplicacion a operaciones basicas

--- operaciones_basicas.py
+++ operaciones_basicas.py
@@ -2,3 +2,8 @@ def suma(a, b):
     """Suma dos números."""
     return a + b
 print("Suma:", suma(5, 3))
+
+def multiplicacion(a, b):
+    """Multiplica dos números."""
+    return a * b
+print("Multiplicación:", multiplicacion(5, 3))
